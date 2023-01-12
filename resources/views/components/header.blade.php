74397d432d6fc98d22bad42c5cca461ce98f49b3
Daniel <angelicodaniel93@gmail.com>
2023-01-12 16:18:47 +0100

    prime prove

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,39 @@
+<img src="../assets/img/dc-logo.png" alt="DC-logo">
+    <nav>
+        <ul>
+          <li>
+            <a href="#">CHARACTERS</a>
+          </li>
+          <li class="active">
+            <a href="#">COMICS</a>
+          </li>
+          <li>
+            <a href="#">MOVIES</a>
+          </li>
+          <li>
+            <a href="#">TV</a>
+          </li>
+          <li>
+            <a href="#">GAMES</a>
+          </li>
+          <li>
+            <a href="#">COLLECTIBLES</a>
+          </li>
+          <li>
+            <a href="#">SHOP</a>
+          </li>
+          <li>
+            <a href="#">VIDEOS</a>
+          </li>
+          <li>
+            <a href="#">FANS</a>
+          </li>
+          <li>
+            <a href="#">NEWS</a>
+          </li>
+          <li>
+            <a href="#">SHOP</a>
+          </li>
+
+      </ul>
+    </nav>
